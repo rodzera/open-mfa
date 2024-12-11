2ebf1509a5e62e5b2edf7a76e8a5e40516b11803
rodzera <rodnascimentolopes@gmail.com>
2024-12-10 23:40:15 -0300

    fix: logs dir not created before gunicorn start; add: latest tag in Makefile

--- src/app/utils/helpers/logs.py
+++ src/app/utils/helpers/logs.py
@@ -1,7 +1,12 @@
+from os import path, makedirs
 from logging import getLogger, Logger
 
 __all__ = ["get_logger"]
 
+
+_log_dir = "./logs"
+if not path.exists(_log_dir):
+    makedirs(_log_dir)
 
 def get_logger(name: str) -> Logger:
     return getLogger("open-mfa." + name) if not name.startswith("redis") else getLogger("redis")
